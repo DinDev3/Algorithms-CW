24a9ab831323a5c55e006ada870d678e531bfc73
Dinuka <drpiyadigama@gmail.com>
2020-03-15 03:06:56 +0530

    Modified the structure of comments in code

--- AlgoCW_MaxFlow/src/lk/dinuka/MaxFlowProblem/MaxFlow.java
+++ AlgoCW_MaxFlow/src/lk/dinuka/MaxFlowProblem/MaxFlow.java
@@ -39,6 +39,7 @@ public class MaxFlow {
                 if (!visited[v] && residualGraph[u][v] > 0) {
                     // if the node hasn't been visited previously and
                     // if a capacity exists outward from a node
+
                     // rGraph[][] index = Node
                     // Eg: rGraph[0][1] = Outward Capacity of Edge from 0 to 1
 
@@ -55,18 +56,19 @@ public class MaxFlow {
     }
 
 
-    // Returns tne maximum flow from s to t in the given graph
+    /* Returns tne maximum flow from s to t in the given graph */
     int fordFulkerson(int[][] graph, int source, int sink) {
-        int u, v;
+        int u;      // starting node
+        int v;      // ending node
 
-        // Create a residual graph and fill the residual graph
-        // with given capacities in the original graph as
-        // residual capacities in residual graph
+        /* Create a residual graph and fill the residual graph
+            with given capacities in the original graph as
+            residual capacities in residual graph */
 
-        // Residual graph where rGraph[i][j] indicates
-        // residual capacity of edge from i to j (if there
-        // is an edge. If rGraph[i][j] is 0, then there is
-        // not)
+        /* Residual graph where rGraph[i][j] indicates
+            residual capacity of edge from i to j (if there
+            is an edge. If rGraph[i][j] is 0, then there is
+            not) */
         int[][] residualGraph = new int[totalVertices][totalVertices];     // initializing residual graph
 
         for (u = 0; u < totalVertices; u++)
@@ -74,16 +76,16 @@ public class MaxFlow {
                 // initial residual capacities are equal to the capacities of the edges
                 residualGraph[u][v] = graph[u][v];
 
-        // This array is filled by BFS and used to store the residual path
+        /* This array is filled by BFS and used to store the residual path */
         int[] parent = new int[totalVertices];
         // initialized with V because the max path can have all the number of vertices in the graph (same node can't be visited more than once)
 
-        int maxFlow = 0;  // There is no flow initially
+        int maxFlow = 0;  /* There is no flow initially */
 
-        // Augment the flow while there is path from source to sink
+        /* Augment the flow while there is path from source to sink */
         while (bfs(residualGraph, source, sink, parent)) {
-            // Find minimum residual capacity of the edges along the path filled by BFS.
-            // Or we can say; find the maximum flow through the path found.
+            /* Find minimum residual capacity of the edges along the path filled by BFS.
+                Or we can say; find the maximum flow through the path found. */
             int pathFlow = Integer.MAX_VALUE;          // assigning the maximum value an integer can have - because minimum residual capacity is needed to be found
             for (v = sink; v != source; v = parent[v]) {
 //                v = parent[v]       // since this is the residual path, the previous visited node(u) is taken after v
@@ -92,27 +94,26 @@ public class MaxFlow {
                 pathFlow = Math.min(pathFlow, residualGraph[u][v]);      // getting the minimum value out of path_flow and rGraph[u][v] (capacity of edge in residual graph)
             }
 
-            // update residual capacities of the edges and reverse edges along the path
+            /* update residual capacities of the edges and reverse edges along the path */
             for (v = sink; v != source; v = parent[v]) {
                 u = parent[v];
                 residualGraph[u][v] -= pathFlow;              // decreasing capacity from the flow sent; to get the residual capacity (forward direction)
                 residualGraph[v][u] += pathFlow;              // adding the flow sent to the reverse capacity; to get the residual capacity (backward direction)
             }
 
-            // Add path flow to overall flow
+            /* Add path flow to overall flow */
             maxFlow += pathFlow;
         }
 
-        // Return the overall flow
+        /* Return the overall flow */
         return maxFlow;
     }
 
 
 
-    // Driver program to test above functions
+    /* Driver program to test above functions */
     public static void main(String[] args) {
-        // Let us create a graph shown in the above example
-        // 2-D array is used
+        // 2-D array is used to create the graph of the flow network
         // graph[][] index = Node
         // Eg: graph[0][2] = Outward Capacity of Edge from node 0 to node 2
         int[][] graph = new int[][]{{0, 16, 13, 0, 0, 0},
@@ -124,7 +125,7 @@ public class MaxFlow {
         };
         MaxFlow m = new MaxFlow();
 
-        System.out.println("The maximum possible flow is " +
+        System.out.println("The Maximum Possible Flow is: " +
                 m.fordFulkerson(graph, 0, 5));
 
     }
