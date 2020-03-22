dbee8a862e379f2f3177fa09beae5ef285473591
Dinuka <drpiyadigama@gmail.com>
2020-03-23 01:36:11 +0530

    Created graph GUI

--- AlgoCW_MaxFlow/src/lk/dinuka/MaxFlowProblem/GraphGUI.java
+++ AlgoCW_MaxFlow/src/lk/dinuka/MaxFlowProblem/GraphGUI.java
@@ -1,5 +1,98 @@
 package lk.dinuka.MaxFlowProblem;
 
-public class GraphGUI {
+import com.mxgraph.layout.mxCircleLayout;
+import com.mxgraph.swing.mxGraphComponent;
+import org.jgrapht.ListenableGraph;
+import org.jgrapht.ext.JGraphXAdapter;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultListenableGraph;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphGUI extends
+        JApplet {
+
+    private static final Dimension DEFAULT_SIZE = new Dimension(530, 320);
+
+    private JGraphXAdapter<String, DefaultEdge> jgxAdapter;
+
+    static int[][] graph;
+    static int totalVertices;
+
+    public static void main(String[] args) {
+
+        graph = new int[][]{{0, 16, 13, 0, 0, 0},
+                {0, 0, 10, 12, 0, 0},
+                {0, 4, 0, 0, 14, 0},
+                {0, 0, 9, 0, 0, 20},
+                {0, 0, 0, 7, 0, 4},
+                {0, 0, 0, 0, 0, 0}
+        };
+        totalVertices = 6;
+
+
+        GraphGUI applet = new GraphGUI();
+        applet.init();
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(applet);
+        frame.setTitle("JGraphT Adapter to JGraphX Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void init() {
+        // create a JGraphT graph
+        ListenableGraph<String, DefaultEdge> g =
+                new DefaultListenableGraph<>(new DefaultDirectedGraph<>(DefaultEdge.class));
+
+        // create a visualization using JGraph, via an adapter
+        jgxAdapter = new JGraphXAdapter<>(g);
+
+        setPreferredSize(DEFAULT_SIZE);
+        mxGraphComponent component = new mxGraphComponent(jgxAdapter);
+        component.setConnectable(false);
+        component.getGraph().setAllowDanglingEdges(false);
+        getContentPane().add(component);
+        resize(DEFAULT_SIZE);
+
+
+        // displaying a graph.
+        // Need to show capacities~~~~
+        // Can't show capacities in both directions~~~~??
+
+        // creating all the nodes of the graph
+        for (int i = 0; i < totalVertices; i++) {
+            g.addVertex(Integer.toString(i));
+        }
+
+        // creating all the edges that exists with Capacities
+        for (int u = 0; u < totalVertices; u++) {
+            int[] startingNode = graph[u];
+            for (int v = 0; v < totalVertices; v++) {
+                if (startingNode[v] > 0) {     // if a capacity exists
+                    g.addEdge(Integer.toString(u), Integer.toString(v));
+                }
+
+            }
+        }
+
+
+        // positioning via jgraphx layouts
+        mxCircleLayout layout = new mxCircleLayout(jgxAdapter);
+
+        // center the circle
+        int radius = 100;
+        layout.setX0((DEFAULT_SIZE.width / 2.0) - radius);
+        layout.setY0((DEFAULT_SIZE.height / 2.0) - radius);
+        layout.setRadius(radius);
+        layout.setMoveCircle(true);
+
+        layout.execute(jgxAdapter.getDefaultParent());
+    }
 
 }
